6b09a3baff22843531b14d092f4ec2d1b059f3fd
Dawit Melka <davebadeg@gmail.com>
2024-06-07 16:05:35 +0300

    c

--- biocypher_metta/adapters/refseq_closest_gene_adapter.py
+++ biocypher_metta/adapters/refseq_closest_gene_adapter.py
@@ -59,7 +59,7 @@ class RefSeqClosestGeneAdapter(Adapter):
                             yield source_id, target_id, self.label, props
 
                         except Exception as e:
-                            logger.error(f"error while parsing row: {row}, error: {e} skipping...")
+                            # logger.error(f"error while parsing row: {row}, error: {e} skipping...")
                             continue
                 except KeyError as e:
                     # logger.error(f"rsid {rsid} not found in dbsnp_rsid_map, skipping...")
